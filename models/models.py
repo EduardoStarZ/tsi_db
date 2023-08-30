d703672d13446f11c5e98012942e20fa072299e9
EduardoStarZ <pathver@gmail.com>
2023-08-30 16:08:52 -0300

    addded database models

--- models/models.py
+++ models/models.py
@@ -2,21 +2,27 @@ from django.db import models
 
 # Create your models here.
 
-class Request(models.Model):
-    archivistName = models.CharField(max_length=255)
-    senderName = models.CharField(max_length=255)
-    filesNames = getFilesSent()
+class File(models.Model):
+    id = models.UUIDField(primary_key=True)
+    fileName = models.CharField(max_length=355)
+    senderName = models.CharField(max_length=355)
+    contentName = models.CharField(max_length=100)
+    size = models.IntegerField()
+    CuratorName = models.CharField(max_length=355)
+    CuratorID = models.ForeignKey(models.Curator, verbose_name= "Curator ID", on_delete= models.CASCADE)
+    semester
+
+class Curator(models.Model):
+    id = models.UUIDField(primary_key=True)
+    name = models.CharField(max_length=300)
+    email = models.CharField(max_length=300)
+    password = models.BinaryField()
+    decryptKey = models.BinaryField()
     
-    def getFilesSent(request):
-        fileNames = []
-        fileSizes = []
-        fileSender = []
-        fileClass = []
-        
-        for x in request:
-            fileNames.append(request.Names[x])
-            fileSizes.append(request.Names[x])
-            fileSender.append(request.Names[x])
-            fileClass.append(request.Names[x])
-            
-        return files
+class Content(models.Model):
+    semester_choices = [
+        1,2,3,4,5
+    ]
+    id = models.UUIDField(primary_key=True)
+    name = models.CharField(max_length=100)
+    semester = models.IntegerChoices(choices = semester_choices)
