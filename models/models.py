20352750a05fc536e434368a64d9981b052abee0
EduardoStarZ <pathver@gmail.com>
2023-08-30 21:45:17 -0300

    finished database modelling

--- models/models.py
+++ models/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from datetime import date
 
 # Create your models here.
 
@@ -10,19 +11,27 @@ class File(models.Model):
     size = models.IntegerField()
     CuratorName = models.CharField(max_length=355)
     CuratorID = models.ForeignKey(models.Curator, verbose_name= "Curator ID", on_delete= models.CASCADE)
-    semesterID = models.ForeignKey(models.content, verbose_name= "Semester ID", on_delete=models.CASCADE) 
+    semesterID = models.ForeignKey(models.content, verbose_name= "Semester ID", on_delete=models.CASCADE)
+    uploadDate = models.DateTimeField()
 
 class Curator(models.Model):
     id = models.UUIDField(primary_key=True)
     name = models.CharField(max_length=300)
-    email = models.CharField(max_length=300)
+    email = models.EmailField()
     password = models.BinaryField()
     decryptKey = models.BinaryField()
     
 class Content(models.Model):
     semester_choices = [
-        1,2,3,4,5
+        ('1', 'First'),
+        ('2', 'Second'),
+        ('3', 'Third'),
+        ('4', 'Fourth'),
+        ('5', 'Fifth'),
+        ('6', 'Optatives'),
+        ('7', 'Extra')
     ]
     id = models.UUIDField(primary_key=True)
     name = models.CharField(max_length=100)
     semester = models.IntegerChoices(choices = semester_choices)
+    
