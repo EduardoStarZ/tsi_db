ac078cd7d324ed6d5a982ad6d26685151fd2ab36
EduardoStarZ <pathver@gmail.com>
2023-11-20 09:18:35 -0300

    changed database structure

--- tsi_db/settings.py
+++ tsi_db/settings.py
@@ -33,7 +33,6 @@ ALLOWED_HOSTS = ['*']
 INSTALLED_APPS = [
     'models',
     'widget_tweaks',
-    'django_htmx',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -43,7 +42,6 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    'django_htmx.middleware.HtmxMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
