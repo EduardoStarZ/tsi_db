4520ecc7d3e589f331928a0fd13a1eaf9046bf25
EduardoStarZ <pathver@gmail.com>
2024-01-11 22:18:54 -0300

    spread the app into pages and models to prevent some abstraction issues

--- models/views.py
+++ models/views.py
@@ -1,37 +1,8 @@
-from django.shortcuts import render, redirect
-from django.http import HttpResponse
-from django.template import loader
-from .forms import FileForm
+from django.shortcuts import render
 from .models import File
 from django.db.models import Q
 # Create your views here.
 
-def main(request):
-    content = File.objects.all()
-    template = 'index.html'
-    context = {
-        'content': content
-    }
-    return render(request, template, context)
-
-def admin(request):
-    
-    file_form = FileForm()
-    content = File.objects.all()
-    
-    if request.method == 'POST':
-        file_form = FileForm(request.POST)
-    
-        if file_form.is_valid():
-            file_form.save()
-            return redirect("admin")
-    
-    template = 'admin.html'
-    context = {
-        'file_form': file_form,
-        'content': content
-    }
-    return render(request, template, context)
 
 def search(request):
     query = request.GET.get("q")
--- pages/views.py
+++ pages/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from models.models import File
+from forms import FileForm
 
 # Create your views here.
+
+def main(request):
+    content = File.objects.all()
+    template = 'index.html'
+    context = {
+        'content': content
+    }
+    return render(request, template, context)
+
+def admin(request):
+    
+    file_form = FileForm()
+    content = File.objects.all()
+    
+    if request.method == 'POST':
+        file_form = FileForm(request.POST)
+    
+        if file_form.is_valid():
+            file_form.save()
+            return redirect("admin")
+    
+    template = 'admin.html'
+    context = {
+        'file_form': file_form,
+        'content': content
+    }
+    return render(request, template, context)
