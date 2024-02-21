13d6d9f7af89c046e4322a3855568b24093a079b
EduardoStarZ <pathver@gmail.com>
2024-02-20 22:24:59 -0300

    refactored url handling through apps

--- models/urls.py
+++ models/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from models import views
 
 urlpatterns = [
-        
+        path('search/', views.search),
+        path('admin_search/', views.admin_search)
 ]
---
+++ scripts/run.bat
@@ -0,0 +1,3 @@
+start 127.0.0.1
+
+python manage runserver
